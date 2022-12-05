24f07cc9d729c37c13aa48bea22b368cd24576d7
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2022-12-05 10:12:15 +0700

    first weacare commit

---
+++ app/src/main/java/com/uc/weacare/Onboarding1.kt
@@ -0,0 +1,11 @@
+package com.uc.weacare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Onboarding1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding1)
+    }
+}
